240dc5bcf1ecc287ffa5faffcbd7b5ad1a4ceb9b
kaluza.simon@gmail.com <kaluza.simon@gmail.com>
2015-01-07 11:07:32 -0500

    Initialized the cookbook with a working integration testing cookbook similar to the "application" cookbook setup to prepare for further development

---
+++ Berksfile
@@ -0,0 +1,10 @@
+source "https://supermarket.getchef.com"
+
+metadata
+
+cookbook "minitest-handler"
+cookbook "application"
+
+group :test do
+  cookbook "application_ember_test", path: "./test/cookbooks/application_ember_test"
+end
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'application_ember'
+maintainer       'Simon Kaluza'
+maintainer_email 'simon@inboxhealth.com'
+license          'All rights reserved'
+description      'Installs/Configures application_ember'
+long_description 'Installs/Configures application_ember'
+version          '0.1.0'
+
